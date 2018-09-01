5c9d2dd3642f072bc51f2e79df30287d5aa58ff1
三宮大輝 <mekutei0@gmail.com>
2018-09-01 22:07:50 +0900

    no message

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 三宮大輝 on 2018/09/01.
+//  Copyright © 2018年 三宮大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
